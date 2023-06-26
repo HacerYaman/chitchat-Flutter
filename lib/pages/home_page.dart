c7447c8371c2814ec92a1988eff1b7d408efe5f2
haceryaman <yaman.hacer@outlook.com>
2023-06-26 15:56:54 +0300

    create account

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:chitchat/services/auth/auth_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import 'chat_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,13 +14,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  void signOut(){
-    final authService= Provider.of<AuthService>(context, listen: false);
-
+  void signOut() {
+    final authService = Provider.of<AuthService>(context, listen: false);
     authService.signOut();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -24,9 +27,58 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("main paeg"),
         actions: [
-          IconButton(onPressed: signOut, icon: Icon(Icons.logout_rounded),)
+          IconButton(
+            onPressed: signOut,
+            icon: Icon(Icons.logout_rounded),
+          )
         ],
       ),
+      body: _buildUserList(),
     );
+  }
+
+  Widget _buildUserList() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection("users").snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text("error");
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        }
+
+        return ListView(
+          children: snapshot.data!.docs
+              .map<Widget>((doc) => _buildUserListItem(doc))
+              .toList(),
+        );
+      },
+    );
+  }
+
+  Widget _buildUserListItem(DocumentSnapshot documentSnapshot) {
+    Map<String, dynamic> data =
+        documentSnapshot.data()! as Map<String, dynamic>;
+
+    if (_auth.currentUser!.email != data["email"]) {
+      return ListTile(
+        title: Text(data["email"]),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ChatPage(
+                receiverUserEmail: data["email"],
+                receiverUserID: data["uid"],
+              ),
+            ),
+          );
+        },
+      );
+    }else{
+      return Container();
+    }
   }
 }
--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,14 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 
 class AuthService extends ChangeNotifier {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
 
   Future<UserCredential> signInWithEmailandPassword(
       String email, String password) async {
     try {
       UserCredential userCredential = await _firebaseAuth
           .signInWithEmailAndPassword(email: email, password: password);
+
+      //bi ihtimal eğer kullanının docu yoksa oluştur (elle kullanıcı eklersem diye)
+
+      _firebaseFirestore.collection("users").doc(userCredential.user!.uid).set({
+        'uid': userCredential.user!.uid,
+        "email": email,
+        "password": password,
+      });
 
       return userCredential;
     } on FirebaseAuthException catch (e) {
@@ -25,6 +35,14 @@ class AuthService extends ChangeNotifier {
     try {
       UserCredential userCredential = await _firebaseAuth
           .createUserWithEmailAndPassword(email: email, password: password);
+
+      //create a file for users collection
+
+      _firebaseFirestore.collection("users").doc(userCredential.user!.uid).set({
+        'uid': userCredential.user!.uid,
+        "email": email,
+        "password": password,
+      });
 
       return userCredential;
     } on FirebaseAuthException catch (e) {
